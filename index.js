b3adeb8a8050d3c3a51eb1d05ff16eb2109a9a52
saadawan7 <163626918+saadawan7@users.noreply.github.com>
2024-06-01 05:57:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+console.log("Wellcome to Codewithsaad - CLI Number Gussing Game");
+let myTasks = [];
+let isRunning = true;
+while (isRunning) {
+    const answers = await inquirer.prompt([
+        {
+            name: "option",
+            type: "list",
+            message: "Select your option",
+            choices: ["Add new task",
+                "View Tasks",
+                "Exit"]
+        }
+    ]);
+    switch (answers.option) {
+        case "Add new task":
+            const newTask = await inquirer.prompt([
+                {
+                    name: "task",
+                    type: "input",
+                    message: "Enter your task"
+                }
+            ]);
+            myTasks.push(newTask.task);
+            console.log('Added new task:', newTask.task);
+            break;
+        case "View Tasks":
+            console.table(myTasks);
+            break;
+        case "Exit":
+            console.log("Exit");
+            isRunning = false;
+            break;
+        default:
+            console.log("Invalid option");
+            break;
+    }
+}
